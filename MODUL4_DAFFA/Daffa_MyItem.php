bbad2b1fa27ffc4c7fe4384953dce7d5110e43bc
daffalhanif <daffalhanif@student.telkomuniversity.ac.id>
2022-11-27 21:26:46 +0700

    modul4

---
+++ MODUL4_DAFFA/Daffa_MyItem.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">   
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</head>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="height: 64px;">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav  gap-2">
+                    <a class="nav-link active" aria-current="page" href="Daffa_Home.php">Home</a>
+                    <a class="nav-link" href="Daffa_AddItem.php">MyCar</a>
+                </div>
+                <a href="Daffa_AddItem.php" class="btn me-md-2" style="margin-left: 1000px; width: 110px; background-color: white; color:blue; font-weight: 600;">Add Car</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <b><br><h2 style="text-align:left">My Show Room</h2></b>
+        <p class="fw-light" style="text-align:left">List Show Room DAFFA - 1202204160</p>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 30rem;">
+                    <div class="row">
+                        <div class="col">
+                        <img src="Civic.jpg" class="card-img-top" style="width: 30rem;">
+                          <div class="card-body">
+                            <h5 class="card-title">Civic Type-R</h5>
+                            <p class="card-text">Honda Civic di Indonesia terkenal sebagai mobil sedan 4 atau 3 pintu, yang menawarkan performa, handling yang tajam dan kenyamanan khas Honda.</p>
+                            <a href="#" class="btn btn-primary">Detail</a>
+                            <button type="button" class="btn btn-danger">Delete</button>
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 30rem;">
+                    <div class="row">
+                        <div class="col">
+                        <img src="Jazz.jpg" class="card-img-top"  height="270px" width="30px">
+                          <div class="card-body">
+                            <h5 class="card-title">Jazz</h5>
+                            <p class="card-text">Honda Jazz adalah mobil hatchback 5 pintu produksi pabrikan otomotif Jepang Honda Motor Company. Honda Jazz pertama kali diperkenalkan pada 2001.</p>
+                            <a href="#" class="btn btn-primary">Detail</a>
+                            <button type="button" class="btn btn-danger">Delete</button> 
+                        </div>
+                        </div>           
+                    </div>
+                </div>
+            </div>
+
+    <div class="container">
+            <div class="col">
+                <div class="card" style="width: 30rem;">
+                    <div class="row">
+                        <div class="col">
+                            <img src="Rush.png" class="card-img-top" alt="Rush" />
+                            <div class="card-body">
+                                <h5 class="card-title">Rush</h5>
+                                <p class="card-text">Toyota Rush adalah 7 Seater SUV yang masih menjadi favorit masyarakat Indonesia untuk mobil segmen Low Sport Utility Vehicle (L-SUV).</p>
+                                <a href="#" class="btn btn-primary">Detail</a>
+                                <button type="button" class="btn btn-danger">Delete</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+    <div class="col">
+    <br><br><p>Jumlah Mobil: 3</p>
+    </div>
